5fd8036cbd1e01900686c626026b3c6367333fa8
a-n-u-b-i-s <watalanubhava@gmail.com>
2023-01-15 21:12:10 -0500

    fix: final env var naming issue

--- src/functions/matching/index.ts
+++ src/functions/matching/index.ts
@@ -2,7 +2,7 @@ import { SQSEvent } from 'aws-lambda';
 import { eventsJson } from 'common/middleware';
 import twilio from 'twilio';
 
-const { TWILIO_SID, TWILIO_AUTH, TWILIO_PHONE_NUMBER, MATCHING_GRID_ID } = process.env;
+const { TWILIO_SID, TWILIO_AUTH, TWILIO_PHONE_NUMBER, MATCHES_GRID_ID } = process.env;
 
 const client = twilio(TWILIO_SID, TWILIO_AUTH);
 
@@ -10,7 +10,7 @@ const conversation = async (event: SQSEvent): Promise<void> => {
 	const sqsBody = JSON.parse(event.Records[0].body);
 
 	await client.messages.create({
-		body: `Message from matching function to ${sqsBody.From} | ${MATCHING_GRID_ID}`,
+		body: `Message from matching function to ${sqsBody.From} | ${MATCHES_GRID_ID}`,
 		from: `${TWILIO_PHONE_NUMBER}`,
 		to: sqsBody.From
 	});
